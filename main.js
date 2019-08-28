790dbf2746eaca291ed87c08e874a3cab49292f0
VikinGSpb <imbot@inbox.ru>
2019-08-29 02:10:15 +0300

    workpieces

---
+++ main.js
@@ -0,0 +1,17 @@
+class AbsCharacter{
+    constructor(name){
+        this._name=name;
+    }
+}
+
+class Player extends AbsCharacter{
+
+}
+
+class Npc extends AbsCharacter{
+
+}
+
+class Enemy extends AbsCharacter{
+    
+}
